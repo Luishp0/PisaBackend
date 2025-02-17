2cffb7e0d09060cfb9ac12ae3291439cb807064e
Luishp0 <soy.luis23@gmail.com>
2025-02-17 17:35:53 -0600

    incorporando .gitignore

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get('/', getAllUsers); // Obtener todos los usuarios
 router.get('/buscar/:nombre', getUserByName); // Buscar usuarios por nombre
 router.get('/estado/:id', getUserStatus); // Obtener estado del usuario
-router.post('/registrar', registerUser);
+router.post('/registrar', registerUser); // Registrar usuario
 
 export default router;
