85c0b927ef381d7ab147461777ef13633569c1d3
Luishp0 <soy.luis23@gmail.com>
2025-02-17 20:09:24 -0600

    creacion de controlador y rutas de roles

--- src/controllers/roleController.js
+++ src/controllers/roleController.js
@@ -22,3 +22,54 @@ export const getRoleByClase = async (req, res) => {
     }
   };
   
+  // Crear un nuevo role
+export const createRole = async (req, res) => {
+  try {
+    const { tipo } = req.body;
+    const newRole = new Role({ tipo });
+    await newRole.save();
+    res.status(201).json(newRole);
+  } catch (error) {
+    res.status(500).json({ message: "Error al crear el rol", error });
+  }
+};
+
+
+// Obtener un role por ID
+export const getRoleById = async (req, res) => {
+  try {
+    const role = await Role.findById(req.params.id);
+    if (!role) {
+      return res.status(404).json({ message: "Role no encontrado" });
+    }
+    res.status(200).json(role);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener el rol", error });
+  }
+};
+
+// Actualizar un role
+export const updateRole = async (req, res) => {
+  try {
+    const role = await Role.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!role) {
+      return res.status(404).json({ message: "Role no encontrado" });
+    }
+    res.status(200).json(role);
+  } catch (error) {
+    res.status(500).json({ message: "Error al actualizar el rol", error });
+  }
+};
+
+// Eliminar un role
+export const deleteRole = async (req, res) => {
+  try {
+    const role = await Role.findByIdAndDelete(req.params.id);
+    if (!role) {
+      return res.status(404).json({ message: "Role no encontrado" });
+    }
+    res.status(200).json({ message: "Role eliminado exitosamente" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al eliminar el rol", error });
+  }
+};
--- src/routes/roleRouter.js
+++ src/routes/roleRouter.js
@@ -1,10 +1,25 @@
 import express from 'express';
-import { getAllRoles, getRoleByClase } from '../controllers/roleController.js';
+import { getAllRoles, getRoleByClase, 
+    createRole,
+    getRoleById,
+    updateRole,
+    deleteRole } from '../controllers/roleController.js';
 
 const router = express.Router();
 
 router.get('/', getAllRoles);
 router.get('/:clase', getRoleByClase);
 
+// Ruta para crear un nuevo role
+router.post("/", createRole);
+
+// Ruta para obtener un role por su ID
+router.get("/:id", getRoleById);
+
+// Ruta para actualizar un role por su ID
+router.put("/:id", updateRole);
+
+// Ruta para eliminar un role por su ID
+router.delete("/:id", deleteRole);
 
 export default router;
