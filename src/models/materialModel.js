cf19f78cf5fcf7e3bae802e4745d387e8cae6812
Luishp0 <soy.luis23@gmail.com>
2025-03-27 02:39:56 -0600

    modificacion del modelo material

--- src/models/materialModel.js
+++ src/models/materialModel.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const MaterialSchema = new mongoose.Schema({
     produccion: { type: mongoose.Schema.Types.ObjectId, ref: 'Produccion', required: true },
     nombreMaterial:{type: String},
-    lote: { type: Number, required: true },
+    lote: { type: String, required: true },
     orden: { type: Number, required: true },
     descripcionMaterial: { type: String, required: true },
     velocidadNominal: {type: Number, required: true }
